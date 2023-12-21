3245ff68fbaa1a2597ac386d4ecec9bf60c42a71
yujhenchen <yujhen.chen93@gmail.com>
2023-12-21 19:00:14 +0800

    use background image instead

--- src/Hero.js
+++ src/Hero.js
@@ -2,16 +2,16 @@ import React from "react";
 
 export default function Hero({ backgroundImage, date, title, body }) {
   return (
-    <div className="hero">
-      <div className="image-container">
-        <img src={backgroundImage} alt="Hero Background Image" />
-      </div>
-
-      <div className="hero-content">
-        <p className="hero-date">{date}</p>
-        <p className="hero-title">{title}</p>
-        <p className="hero-body">{body}</p>
-      </div>
+    <div
+      className="hero"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundPosition: "center",
+      }}
+    >
+      <p className="hero-date">{date}</p>
+      <p className="hero-title">{title}</p>
+      <p className="hero-body">{body}</p>
     </div>
   );
 }
